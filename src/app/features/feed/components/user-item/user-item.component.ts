531b1a133b02986ed4f4123e4d74f2dce9790061
Abdul Halim <ahr.web.pro@gmail.com>
2023-08-02 03:12:39 +0600

    Implement follow user feature partially done

--- src/app/features/feed/components/user-item/user-item.component.ts
+++ src/app/features/feed/components/user-item/user-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { Subscribable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from './../../services/user.service';
+import { FeedService } from '../../services/feed.service';
 
 @Component({
   selector: 'app-user-item',
@@ -7,4 +11,44 @@ import { Component, Input } from '@angular/core';
 })
 export class UserItemComponent {
   @Input() user: any;
+
+  userId!: number;
+  isFollowed: boolean = false;
+
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService,
+    private feedService: FeedService
+  ) {}
+
+  ngOnInit(): void {
+    this.feedService.getNextTweetsPage().subscribe((item) => {
+      this.userId = item[0];
+    });
+  }
+
+  followUser() {
+    const data: any = {
+      user_id: this.userId,
+    };
+
+    console.log(this.userId);
+
+    const tweetSubscribable: Subscribable<any> =
+      this.userService.followUser(data);
+
+    tweetSubscribable.subscribe({
+      next: (response) => {
+        if (response.resp) {
+          this.isFollowed = true;
+          this.toastr.success(response.message);
+        }
+      },
+      error: (err) => {
+        this.toastr.error('Failed to follow user');
+      },
+    });
+  }
+
+  unfollowUser() {}
 }
--- src/app/features/feed/services/user.service.ts
+++ src/app/features/feed/services/user.service.ts
@@ -1,3 +1,4 @@
+import { FeedService } from './feed.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
@@ -9,8 +10,9 @@ import { environment } from 'src/environments/environment';
 export class UserService {
   private currentPage = 1;
   private pageSize = 30;
+  tweets: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private feedService: FeedService) {}
 
   getNextUsersPage(): Observable<any[]> {
     const url = `${environment.BASE_API_URL}/users?page=${this.currentPage}&size=${this.pageSize}`;
@@ -28,5 +30,11 @@ export class UserService {
     return this.http.get<any>(
       `${environment.BASE_API_URL}/users?page=${page}&size=${size}`
     );
+  }
+
+  followUser(userId: number): Observable<any> {
+    console.log(userId, `${environment.BASE_API_URL}/follow`);
+    
+    return this.http.post<any>(`${environment.BASE_API_URL}/follow`, userId);
   }
 }
