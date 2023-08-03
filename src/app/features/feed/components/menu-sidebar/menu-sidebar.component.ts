067583742ee1dfda2e9d58431ce659c6e7fd5b3e
Abdul Halim <ahr.web.pro@gmail.com>
2023-08-03 16:15:43 +0600

    initialize flowbit on menu sidebar component

--- src/app/features/feed/components/menu-sidebar/menu-sidebar.component.ts
+++ src/app/features/feed/components/menu-sidebar/menu-sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 import { AuthService } from '../../../auth/auth.service';
 import { SharedService } from '../../services/shared.service';
 
@@ -8,12 +9,16 @@ import { SharedService } from '../../services/shared.service';
   templateUrl: './menu-sidebar.component.html',
   styleUrls: ['./menu-sidebar.component.css'],
 })
-export class MenuSidebarComponent {
+export class MenuSidebarComponent implements OnInit {
   constructor(
     private router: Router,
     private authService: AuthService,
     private sharedService: SharedService
   ) {}
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 
   postTweet() {
     this.router.navigateByUrl('/home');
